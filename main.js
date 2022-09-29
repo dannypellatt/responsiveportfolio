e0f7244b55b5564ab9effd1ad2d5306b41b1b3f5
dannypellatt <danielpellatt92@gmail.com>
2022-09-29 13:52:57 -0700

    got category and project options working

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 
 
-$(document).ready(function() {
-    $("input[name$='projects']").click(function() {
+$(document).ready(function(){ 
+    var lastBoxOpened = 0;
+    
+    $("input[name$='group1']").click(function() {
+        var boxNumber = parseInt($(this).parents('.desc').attr("data-box"));
+        
+        //check if the data-box was succesfully retrieved. If not, first option chosen, reset all of it
+        if(isNaN(boxNumber)){
+         boxNumber = 0;
+        }
+            
         var test = $(this).val();
-
-        $("div.desc").hide();
-        $("#Project" + test).show();
-    });
+        var target = $("#"+test)
+        var newBoxOpened = target.attr("data-box");
+        //check if the last opened box was an earlier one than the newly clicked one
+        if(lastBoxOpened > boxNumber){
+         //hide boxes beyond the one we opened now
+         $('.desc').each(function(){
+         //if box number is bigger than the currently clicked box, close them.
+          if($(this).attr("data-box") > boxNumber){
+          $(this).hide();
+          //uncheck the previously selected radio buttons in now hidden things
+          $('input', this).prop("checked", false);
+          }
+         });
+        }
+        //render target box
+         target.show();
+         //update last opened box to the newly opened one
+         lastBoxOpened = newBoxOpened;
+    }); 
 });
 
-$(document).ready(function() {
-    $("input[name$='Project1']").click(function() {
-        var test = $(this).val();
 
-        $("div.desc").hide();
-        $("#LargeProject" + test).show();
-    });
+$('.radio_mark_category').on('click', function(){
+    $('.radio_mark_category').removeClass('selected');
+    $('.radio_mark_project').removeClass('selected');
+    $(this).addClass('selected');
+});
+
+$('.radio_mark_project').on('click', function(){
+    $('.radio_mark_project').removeClass('selected');
+    $(this).addClass('selected');
 });
